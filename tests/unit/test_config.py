3279db75080deb6dc713985027a60c1fcd4c753d
Dom Batten <dominic.batten@googlemail.com>
2021-10-29 18:45:53 +0200

    add fixtures for creating `pyproject.toml`

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from typing import Callable
 
 import pytest
-import toml
 from pydantic import ValidationError
 
 from maison.config import ProjectConfig
@@ -37,32 +36,29 @@ class TestProjectConfig:
 
         assert str(config) == "ProjectConfig (config_path=None)"
 
-    def test_to_dict(self, create_tmp_file: Callable[..., Path]) -> None:
+    def test_to_dict(self, create_pyproject_toml: Callable[..., Path]) -> None:
         """
         Given an instance of `ProjectConfig`,
         When the `to_dict` method is invoked,
         Then a dictionary of all config options is returned
         """
-        config_dict = {"tool": {"foo": {"bar": "baz"}}}
-        config_toml = toml.dumps(config_dict)
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml()
 
         config = ProjectConfig(project_name="foo", starting_path=pyproject_path)
 
-        assert config.to_dict() == config_dict["tool"]["foo"]
+        assert config.to_dict() == {"bar": "baz"}
 
 
 class TestGetOption:
     """Tests for the `get_option` method."""
 
-    def test_valid_pyproject(self, create_tmp_file: Callable[..., Path]) -> None:
+    def test_valid_pyproject(self, create_pyproject_toml: Callable[..., Path]) -> None:
         """
         Given a valid `pyproject.toml`,
         When the `ProjectConfig` class is instantiated,
         Then a config value can be retrieved
         """
-        config_toml = toml.dumps({"tool": {"foo": {"bar": "baz"}}})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml()
         config = ProjectConfig(project_name="foo", starting_path=pyproject_path)
 
         result = config.get_option("bar")
@@ -94,15 +90,14 @@ class TestGetOption:
         assert result == "baz"
 
     def test_valid_pyproject_with_no_project_section(
-        self, create_tmp_file: Callable[..., Path]
+        self, create_toml: Callable[..., Path]
     ) -> None:
         """
-        Given a valid `pyproject.toml`,
+        Given a valid `pyproject.toml` without a [tool.{project_name}] section,
         When the `ProjectConfig` class is instantiated,
         Then a config value can be retrieved
         """
-        config_toml = toml.dumps({"foo": "bar"})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_toml(filename="pyproject.toml", content={"foo": "bar"})
         config = ProjectConfig(project_name="baz", starting_path=pyproject_path)
 
         result = config.get_option("foo")
@@ -125,15 +120,14 @@ class TestSourceFiles:
         assert config.to_dict() == {}
 
     def test_single_valid_toml_source(
-        self, create_tmp_file: Callable[..., Path]
+        self, create_pyproject_toml: Callable[..., Path]
     ) -> None:
         """
         Given a `toml` source file other than `pyproject.toml`,
         When the `ProjectConfig` is instantiated with the source,
         Then the source is retrieved correctly
         """
-        config_toml = toml.dumps({"tool": {"foo": {"bar": "baz"}}})
-        source_path = create_tmp_file(content=config_toml, filename="another.toml")
+        source_path = create_pyproject_toml(filename="another.toml")
 
         config = ProjectConfig(
             project_name="foo",
@@ -146,19 +140,17 @@ class TestSourceFiles:
         assert result == "baz"
 
     def test_multiple_valid_toml_sources(
-        self, create_tmp_file: Callable[..., Path]
+        self, create_pyproject_toml: Callable[..., Path]
     ) -> None:
         """
         Given multiple `toml` source files,
         When the `ProjectConfig` is instantiated with the sources,
         Then first source to be found is retrieved correctly
         """
-        config_toml_1 = toml.dumps({"tool": {"foo": {"bar": "baz"}}})
-        source_path_1 = create_tmp_file(content=config_toml_1, filename="another.toml")
+        source_path_1 = create_pyproject_toml(filename="another.toml")
 
-        config_toml_2 = toml.dumps({"tool": {"oof": {"rab": "zab"}}})
-        source_path_2 = create_tmp_file(
-            content=config_toml_2, filename="pyproject.toml"
+        source_path_2 = create_pyproject_toml(
+            section_name="oof", content={"rab": "zab"}
         )
 
         config = ProjectConfig(
@@ -222,7 +214,7 @@ class TestValidation:
 
     def test_one_schema_with_valid_config(
         self,
-        create_tmp_file: Callable[..., Path],
+        create_pyproject_toml: Callable[..., Path],
     ) -> None:
         """
         Given an instance of `ProjectConfig` with a given schema,
@@ -235,8 +227,7 @@ class TestValidation:
 
             bar: str
 
-        config_toml = toml.dumps({"tool": {"foo": {"bar": "baz"}}})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml()
         config = ProjectConfig(
             project_name="foo",
             starting_path=pyproject_path,
@@ -249,7 +240,7 @@ class TestValidation:
 
     def test_use_schema_values(
         self,
-        create_tmp_file: Callable[..., Path],
+        create_pyproject_toml: Callable[..., Path],
     ) -> None:
         """
         Given an instance of `ProjectConfig` with a given schema,
@@ -264,8 +255,7 @@ class TestValidation:
             bar: str
             other: str = "hello"
 
-        config_toml = toml.dumps({"tool": {"foo": {"bar": 1}}})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml(content={"bar": 1})
         config = ProjectConfig(
             project_name="foo",
             starting_path=pyproject_path,
@@ -279,7 +269,7 @@ class TestValidation:
 
     def test_not_use_schema_values(
         self,
-        create_tmp_file: Callable[..., Path],
+        create_pyproject_toml: Callable[..., Path],
     ) -> None:
         """
         Given an instance of `ProjectConfig` with a given schema,
@@ -293,8 +283,7 @@ class TestValidation:
             bar: str
             other: str = "hello"
 
-        config_toml = toml.dumps({"tool": {"foo": {"bar": 1}}})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml(content={"bar": 1})
         config = ProjectConfig(
             project_name="foo",
             starting_path=pyproject_path,
@@ -308,7 +297,7 @@ class TestValidation:
 
     def test_schema_override(
         self,
-        create_tmp_file: Callable[..., Path],
+        create_pyproject_toml: Callable[..., Path],
     ) -> None:
         """
         Given an instance of `ProjectConfig` with a given schema,
@@ -326,8 +315,7 @@ class TestValidation:
 
             bar: str = "schema_2"
 
-        config_toml = toml.dumps({"tool": {"foo": {}}})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml(content={"baz": "baz"})
         config = ProjectConfig(
             project_name="foo",
             starting_path=pyproject_path,
@@ -340,7 +328,7 @@ class TestValidation:
 
     def test_invalid_configuration(
         self,
-        create_tmp_file: Callable[..., Path],
+        create_pyproject_toml: Callable[..., Path],
     ) -> None:
         """
         Given a configuration which doesn't conform to the schema,
@@ -353,8 +341,7 @@ class TestValidation:
 
             bar: str
 
-        config_toml = toml.dumps({"tool": {"foo": {}}})
-        pyproject_path = create_tmp_file(content=config_toml, filename="pyproject.toml")
+        pyproject_path = create_pyproject_toml(content={"baz": "baz"})
         config = ProjectConfig(
             project_name="foo",
             starting_path=pyproject_path,
