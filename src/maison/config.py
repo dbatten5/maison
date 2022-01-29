3a7a29fbf9747a316665fbfcd5f12e100cc93f0b
Jasha <8935917+Jasha10@users.noreply.github.com>
2022-01-29 16:28:59 -0600

    right-to-left precedence

--- src/maison/config.py
+++ src/maison/config.py
@@ -189,24 +189,27 @@ class ProjectConfig:
             return self._sources[0].to_dict()
 
         source_dicts = [source.to_dict() for source in self._sources]
-        return reduce(lambda a, b: deep_merge, source_dicts)
+        return reduce(lambda a, b: deep_merge(a, b), source_dicts)
 
-def deep_merge(source, destination):
+def deep_merge(destination, source):
     """
+    Deep-merge dictionaries, with souce dict taking precedence over destination dict.
+    Note that the inputs may be modified in place.
+
     >>> a = { 'first' : { 'all_rows' : { 'pass' : 'dog', 'number' : '1' } } }
     >>> b = { 'first' : { 'all_rows' : { 'fail' : 'cat', 'number' : '5' } } }
-    >>> merge(b, a) == { 'first' : { 'all_rows' : { 'pass' : 'dog', 'fail' : 'cat', 'number' : '5' } } }
+    >>> deep_merge(a, b) == { 'first' : { 'all_rows' : { 'pass' : 'dog', 'fail' : 'cat', 'number' : '5' } } }
     True
 
-    Taken from https://stackoverflow.com/a/20666342
+    Based on https://stackoverflow.com/a/20666342
     """
-    for key, value in source.items():
-        if isinstance(value, dict):
+    for key, src_value in source.items():
+        if isinstance(src_value, dict):
             # get node or create one
-            node = destination.setdefault(key, {})
-            merge(value, node)
+            dest_node = destination.setdefault(key, {})
+            deep_merge(dest_node, src_value)
         else:
-            destination[key] = value
+            destination[key] = src_value
 
     return destination
 
