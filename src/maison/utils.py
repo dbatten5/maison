a708cfe81799568182a71fdecba501d671e18d02
Jasha <8935917+Jasha10@users.noreply.github.com>
2022-02-01 00:28:58 -0600

    attempt to fix flake8 issues

--- src/maison/utils.py
+++ src/maison/utils.py
@@ -116,20 +116,22 @@ def deep_merge(destination: Dict[Any, Any], source: Dict[Any, Any]) -> Dict[Any,
 
     Args:
         destination: A dictionary to be merged into. This will be updated in place.
-        source: The dictionary supplying data 
+        source: The dictionary supplying data
 
     Returns:
         The updated destination dictionary.
 
     Raises:
         RuntimeError: A dict cannot be merged on top of a non-dict.
-        For example, the following would fail:
-        `deep_merge({"foo": "bar"}, {"foo": {"baz": "qux"}})`
+          For example, the following would fail:
+          `deep_merge({"foo": "bar"}, {"foo": {"baz": "qux"}})`
 
     Usage example:
     >>> a = { 'first' : { 'all_rows' : { 'pass' : 'dog', 'number' : '1' } } }
     >>> b = { 'first' : { 'all_rows' : { 'fail' : 'cat', 'number' : '5' } } }
-    >>> deep_merge(a, b) == { 'first' : { 'all_rows' : { 'pass' : 'dog', 'fail' : 'cat', 'number' : '5' } } }
+    >>> deep_merge(a, b) == {
+    ...     "first": {"all_rows": {"pass": "dog", "fail": "cat", "number": "5"}}
+    ... }
     True
 
     Based on https://stackoverflow.com/a/20666342
