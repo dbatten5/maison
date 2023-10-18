5859c1ee81a84a3f6ddc3b442b68e0e7d2a0e681
Dom Batten <dominic.batten@googlemail.com>
2023-10-18 11:31:21 +0100

    update ruff linter
    - remove linters absorbed by ruff
    - add more rules

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from click.testing import CliRunner
 from maison import __main__
 
 
-@pytest.fixture
+@pytest.fixture()
 def runner() -> CliRunner:
     """Fixture for invoking command-line interfaces."""
     return CliRunner()
