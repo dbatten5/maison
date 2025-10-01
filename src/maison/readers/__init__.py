66a157704b5ba075d64ce736889f4c99c0e93b9d
Dom Batten <dominic.batten@googlemail.com>
2025-10-01 09:42:37 +0100

    Add `pyproject.toml` parser

--- src/maison/readers/__init__.py
+++ src/maison/readers/__init__.py
@@ -1,5 +1,6 @@
 from .ini import IniReader
+from .pyproject import PyprojectReader
 from .toml import TomlReader
 
 
-__all__ = ["IniReader", "TomlReader"]
+__all__ = ["IniReader", "PyprojectReader", "TomlReader"]
