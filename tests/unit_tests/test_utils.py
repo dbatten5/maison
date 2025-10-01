cfb4565780b3e6f693db4d230ce77cc78bc17b29
Dom Batten <dominic.batten@googlemail.com>
2025-10-01 18:22:05 +0100

    Update types in `deep_merge` test

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from maison import types
 from maison.utils import deep_merge
 
 
@@ -12,32 +13,32 @@ class TestDeepMerge:
         ("a", "b", "expected"),
         [
             pytest.param(
-                {1: 2, 3: 4},
-                {3: 5, 6: 7},
-                {1: 2, 3: 5, 6: 7},
+                {"1": "2", "3": "4"},
+                {"3": "5", "6": "7"},
+                {"1": "2", "3": "5", "6": "7"},
                 id="simple",
             ),
             pytest.param(
-                {1: 2, 3: {4: 5, 6: 7}},
-                {3: {6: 8, 9: 10}, 11: 12},
-                {1: 2, 3: {4: 5, 6: 8, 9: 10}, 11: 12},
+                {"1": "2", "3": {"4": "5", "6": "7"}},
+                {"3": {"6": "8", "9": "10"}, "11": "12"},
+                {"1": "2", "3": {"4": "5", "6": "8", "9": "10"}, "11": "12"},
                 id="nested",
             ),
         ],
     )
     def test_success(
         self,
-        a: dict[int, int],
-        b: dict[int, int],
-        expected: dict[int, int],
+        a: types.ConfigValues,
+        b: types.ConfigValues,
+        expected: types.ConfigValues,
     ) -> None:
         assert deep_merge(a, b) == expected
         assert a == expected
 
     def test_incompatible_dicts(self) -> None:
         """Trying to merge incompatible dicts returns an error"""
-        dict_a = {1: 2, 2: 5}
-        dict_b = {1: {3: 4}}
+        dict_a: types.ConfigValues = {"1": "2", "2": "5"}
+        dict_b: types.ConfigValues = {"1": {"3": "4"}}
 
         with pytest.raises(RuntimeError):
-            deep_merge(dict_a, dict_b)
+            _ = deep_merge(dict_a, dict_b)
