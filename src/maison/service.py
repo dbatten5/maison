61e2b908b2551ac9ed79ddfeed0275e0997b22be
Dom Batten <dominic.batten@googlemail.com>
2025-10-01 17:19:00 +0100

    Move protocols into `protocols.py` module

--- src/maison/service.py
+++ src/maison/service.py
@@ -9,64 +9,14 @@ from maison import types
 from maison import utils
 
 
-class Filesystem(typing.Protocol):
-    """Defines the interface for a class that interacts with a filesystem."""
-
-    def get_file_path(
-        self, file_name: str, starting_path: typing.Optional[pathlib.Path] = None
-    ) -> typing.Optional[pathlib.Path]:
-        """Search for a file by traversing up a filesystem from a path.
-
-        Args:
-            file_name: the name of the file or an absolute path to a config to search for
-            starting_path: an optional path from which to start searching
-
-        Returns:
-            The `Path` to the file if it exists or `None` if it doesn't
-        """
-
-
-class ConfigParser(typing.Protocol):
-    """Defines the interface for a class that parses a config."""
-
-    def parse_config(self, file_path: pathlib.Path) -> types.ConfigValues:
-        """Parse a config.
-
-        Args:
-            file_path: the path to a config file.
-
-        Returns:
-            the parsed config
-        """
-        ...
-
-
-class Validator(typing.Protocol):
-    """Defines the interface for a class that validates some config values."""
-
-    def validate(
-        self, values: types.ConfigValues, schema: type[protocols.IsSchema]
-    ) -> types.ConfigValues:
-        """Validate a config.
-
-        Args:
-            values: the config values
-            schema: a schema against which to validate the config values
-
-        Returns:
-            the validated values
-        """
-        ...
-
-
 class ConfigService:
     """The main service class."""
 
     def __init__(
         self,
-        filesystem: Filesystem,
-        config_parser: ConfigParser,
-        validator: Validator,
+        filesystem: protocols.Filesystem,
+        config_parser: protocols.ConfigParser,
+        validator: protocols.Validator,
     ) -> None:
         """Initialize the class.
 
