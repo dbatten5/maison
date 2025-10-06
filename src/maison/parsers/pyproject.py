57c8c7f7a199241a55fec86fb0398f330e632fa5
Dom Batten <dominic.batten@googlemail.com>
2025-10-06 09:20:39 +0100

    Simplify `PyprojectParser`
    Since it's a `toml` file we can inherit from `TomlParser` and delegate
    parsing the file to the parent class.

--- src/maison/parsers/pyproject.py
+++ src/maison/parsers/pyproject.py
@@ -1,18 +1,9 @@
 """A parser for pyproject.toml files."""
 
-import pathlib
-import sys
+from maison.parsers import toml
 
 
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import tomli as tomllib
-
-from maison import typedefs
-
-
-class PyprojectParser:
+class PyprojectParser(toml.TomlParser):
     """Responsible for parsing pyproject.toml files.
 
     Implements the `Parser` protocol
@@ -25,13 +16,4 @@ class PyprojectParser:
             package_name: the name of the package to look for in file, e.g.
                 `acme` part of `[tool.acme]`.
         """
-        self._package_name = package_name
-
-    def parse_config(self, file_path: pathlib.Path) -> typedefs.ConfigValues:
-        """See the Parser.parse_config method."""
-        try:
-            with file_path.open(mode="rb") as fd:
-                pyproject_dict = dict(tomllib.load(fd))
-        except FileNotFoundError:
-            return {}
-        return dict(pyproject_dict.get("tool", {}).get(self._package_name, {}))
+        super().__init__(section_key=("tool", package_name))
